bf5765ba68a1612ad4048d291562f61515002763
luwarnava <128074585+luwarnava@users.noreply.github.com>
2023-08-09 13:32:22 -0300

    :tada:  initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+    const html = document.documentElement
+
+     //if(html.classList.contains('light')) {
+     //   html.classList.remove('light') 
+     // } else {
+     //   html.classList.add('light')}
+  
+     //código pode ser simplificado com o toggle já existente
+
+     html.classList.toggle('light')
+}
+    
